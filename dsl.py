8a5309f3d14593319de63b41fa57bcc839ced28e
legkvla <vladimir.legkunets@gmail.com>
2020-04-15 17:52:19 +0300

    Adding Msg class

--- dsl.py
+++ dsl.py
@@ -8,6 +8,22 @@ import json
 #     def __repr__(self):
 #         return self.toJson()
 
+# (s/defschema Msg
+#   {
+#     :msg_id s/Int ;should be unique on WorldUpdate entity.
+#     :msg_body s/Str})
+
+class Msg(object):
+    """Msg representation"""
+
+    seq_num = 0
+
+    def __init__(self, msg_body):
+        super(Msg, self).__init__()
+        Msg.seq_num += 1
+        self.msg_id = Msg.seq_num
+        self.msg_body = msg_body
+
 class Tile(object):
     """Tile representation"""
 
@@ -20,5 +36,9 @@ class Tile(object):
         self.tile_y = tile_y
 
 t = Tile("green", 1, 1)
+m = Msg("HELLO")
+m2 = Msg("WORLD")
 
 print(t.__dict__)
+print(m.__dict__)
+print(m2.__dict__)
